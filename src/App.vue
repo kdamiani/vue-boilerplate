a95d4b70f7385b142c9db8764a073b9d511f41ba
kdamiani <info@kevin-damiani.de>
2024-07-10 18:20:22 +0300

    feat: add PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div>
+    <Button label="Click me!" />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+// unstyled version
+// app.use(PrimeVue, {
+//   unstyled: true
+// });
 
 app.mount('#app')
